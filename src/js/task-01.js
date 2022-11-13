dc859ffb25ca6abd543b880c595860b9b90d71f8
NEXT1990 <maksim.listunov@gmail.com>
2022-11-14 01:02:40 +0200

    add-task-3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,18 +1,34 @@
 const liEl = document.querySelectorAll(".item");
 console.log("Number of categories:", liEl.length);
 
-const firstLiElChild = liEl[0].firstElementChild;
-console.log("Category:", firstLiElChild.textContent);
-const listAllElements = document.querySelectorAll(".item ul");
-const animalsOfList = listAllElements[0].children.length;
-console.log("Elements:", animalsOfList);
+// liEl.forEach(elements);
 
-const secondLiElChild = liEl[1].firstElementChild;
-console.log("Category:", secondLiElChild.textContent);
-const productslOfList = listAllElements[1].children.length;
-console.log("Elements:", productslOfList);
+// function elements(category) {
+//   const categotyTitles = category.firstElementChild;
+//   console.log("Category:", categotyTitles.textContent);
+//   const animalsOfList = categotyTitles.nextElementSibling;
+//   console.log("Elements:", animalsOfList.childElementCount);
+// }
 
-const lastLiElChild = liEl[2].firstElementChild;
-console.log("Category:", lastLiElChild.textContent);
-const technologieslOfList = listAllElements[2].children.length;
-console.log("Elements:", technologieslOfList);
+for (const element of liEl) {
+  const categotyTitles = element.firstElementChild;
+  console.log("Category:", categotyTitles.textContent);
+  const animalsOfList = categotyTitles.nextElementSibling;
+  console.log("Elements:", animalsOfList.childElementCount);
+}
+
+// const listAllElements = document.querySelectorAll(".item ul");
+// const firstLiElChild = liEl[0].firstElementChild;
+// console.log("Category:", firstLiElChild.textContent);
+// const animalsOfList = listAllElements[0].children.length;
+// console.log("Elements:", animalsOfList);
+
+// const secondLiElChild = liEl[1].firstElementChild;
+// console.log("Category:", secondLiElChild.textContent);
+// const productslOfList = listAllElements[1].children.length;
+// console.log("Elements:", productslOfList);
+
+// const lastLiElChild = liEl[2].firstElementChild;
+// console.log("Category:", lastLiElChild.textContent);
+// const technologieslOfList = listAllElements[2].children.length;
+// console.log("Elements:", technologieslOfList);
