7ff3a02d00695bb1782e646ecb6934702d917852
NEXT1990 <maksim.listunov@gmail.com>
2022-11-17 00:24:49 +0200

    add-task-6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,6 +1,17 @@
 const refs = {
   input: document.querySelector("#validation-input"),
-  dataLength: document.querySelector(`input[data-length="6"]`),
+  dataLength: document.querySelector(`input[data-length="6"]`).dataset.length,
 };
 
-console.log(refs);
+console.log(refs.dataLength);
+
+refs.input.addEventListener("blur", (event) => {
+  if (event.currentTarget.value.length == refs.dataLength) {
+    refs.input.classList.remove("invalid");
+    refs.input.classList.add("valid");
+    return;
+  }
+  refs.input.classList.add("invalid");
+  refs.input.classList.remove("valid");
+  return;
+});
