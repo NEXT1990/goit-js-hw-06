84197b805062fb624d0a347aeb5f92192ae680e0
NEXT1990 <maksim.listunov@gmail.com>
2022-11-18 00:17:50 +0200

    work-on-task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,9 @@
+const form = document.querySelector(".login-form");
+console.log(form);
 
+form.addEventListener("submit", clickOnSubmit);
+
+function clickOnSubmit(event) {
+  event.preventDefault();
+  console.log(`это нажатие на сабмит`);
+}
