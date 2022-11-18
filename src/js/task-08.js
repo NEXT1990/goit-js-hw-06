f9e22d8692165a7484a2c75a99e9f72c0bc829a1
NEXT1990 <maksim.listunov@gmail.com>
2022-11-19 00:16:59 +0200

    add-task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,9 +1,24 @@
 const form = document.querySelector(".login-form");
-console.log(form);
+const inputEmail = form.querySelector("input[type='email']");
+const inputPassword = form.querySelector("input[type='password']");
+console.dir(inputEmail, inputPassword);
 
 form.addEventListener("submit", clickOnSubmit);
 
 function clickOnSubmit(event) {
   event.preventDefault();
-  console.log(`это нажатие на сабмит`);
+  const formElements = event.currentTarget.elements;
+  const email = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (email.length && password.length !== 0) {
+    const dataForm = {
+      email,
+      password,
+    };
+    console.log(dataForm);
+    form.reset();
+    return;
+  }
+  alert(`Всі поля повинні бути заповнені!!!`);
 }
