ea6dde24fb467dda17585a7f7e0d4878a6784f39
NEXT1990 <maksim.listunov@gmail.com>
2022-11-19 01:13:25 +0200

    add-task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector("body"),
+  spanEl: document.querySelector(".color"),
+  buttonChangeColor: document.querySelector(".change-color"),
+};
+
+refs.buttonChangeColor.addEventListener("click", onButtonChangeColorClick);
+
+function onButtonChangeColorClick(event) {
+  refs.spanEl.textContent = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+}
